aa71734abcbde2aa561a89b1a820fd8d4247aed4
Mateo Bramwell <bramwell.mateo@gmail.com>
2016-05-07 20:01:52 -0700

    New room
    Creating a room setup

--- game.py
+++ game.py
@@ -4,7 +4,24 @@ class Game(cmd.Cmd):
   def __init__(self):
     cmd.Cmd.__init__(self)
 			
+  def do_n(self, args):
+      """Go north"""
+      pass
+
+  def do_s(self, args):
+      """Go south"""
+      pass
+
+  def do_e(self, args):
+      """Go east"""
+      pass
+
+  def do_w(self, args):
+      """Go west"""
+      pass         
+      
   def do_quit(self, args):
+    """Leaves the game"""
     print("Thank you for playing")
     return True	
 			
